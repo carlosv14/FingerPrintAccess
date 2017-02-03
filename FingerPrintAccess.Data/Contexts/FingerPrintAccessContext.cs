a415510d25da34e38f34d6d015be88462fed1e6e
Christopher Escalon <chrisescalon@gmail.com>
2017-02-03 16:50:21 -0600

    Entity Framework dependency added

---
+++ FingerPrintAccess.Data/Contexts/FingerPrintAccessContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FingerPrintAccess.Data.Contexts
+{
+    public class FingerPrintAccessContext : DbContext
+    {
+        public FingerPrintAccessContext() 
+            : base("name=FingerPrintAccess")
+        {
+        }
+    }
+}

aa875d2181bfada3f3903faefa768ac6b28fc2b9
carlosv14 <carlos.varela@unitec.edu>
2017-02-04 19:40:24 -0600

    Adds missing untracked file

---
+++ FingerPrintAccess.API/Models/UserFormViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FingerPrintAccess.API.Models
+{
+    public class UserFormViewModel
+    {
+        public UserFormViewModel()
+        {
+            CreationDate = DateTime.Now;
+        }
+        public long Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public DateTime CreationDate { get; private set; }
+    }
+}

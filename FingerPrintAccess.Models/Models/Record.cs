5705b5fe5f0e4236f1cb5945162960c8d14a64c2
Christopher Escalon <christopherescalon@unitec.edu>
2017-02-06 18:02:04 -0600

    Missing not versioned files

---
+++ FingerPrintAccess.Models/Models/Record.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FingerPrintAccess.Models.Models
+{
+    public class Record
+    {
+        public int Id { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public CheckState Check { get; set; }
+
+        public virtual User User { get; set; }
+    }
+
+    public enum CheckState
+    {
+        In,
+        Out
+    }
+}
